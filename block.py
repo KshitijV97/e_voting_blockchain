cb63e2c59a05acba541d5dd1c200723d8d871085
Rohit <puthenrohit@gmail.com>
2018-12-09 08:02:48 +0530

    Check chain fn

--- block.py
+++ block.py
@@ -52,3 +52,48 @@ def check_block_hash(block):
 
     return
 
+def check_block_validity(block, parent, state):
+    parent_number = parent['contents']['block_number']
+    parent_hash = parent['hash']
+    block_number = block['contents']['block_number']
+
+    for transaction in block['contents']['transaction']:
+        if check_transaction(transaction, state):
+            state = update_state(transaction, state)
+        else:
+            raise
+
+    check_block_hash(block)
+
+    if block_number is not parent_number + 1:
+        raise
+
+    if block['contents']['parent_hash'] is not parent_hash:
+        raise
+
+    return state
+
+def check_chain(chain):
+    if type(chain) is str:
+        try:
+            chain = json.loads(chain)
+            assert (type(chain) == list)
+        except ValueError:
+            return False
+    elif type(chain) is not list:
+        return False
+
+    state = {}
+
+    for transaction in chain[0]['contents']['transaction']:
+        state = update_state(transaction, state)
+
+    check_block_hash(chain[0])
+    parent = chain[0]
+
+    for block in chain[1:]:
+        state = check_block_validity(block, parent, state)
+        parent = block
+
+    return state
+
